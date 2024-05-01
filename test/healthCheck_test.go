dcbec4b530bc0f726f9fd050ca1efc3fc2f2421a
dr0th3r <dr0th3r.code@gmail.com>
2024-05-01 08:40:13 +0200

    test: add test for health check

---
+++ test/healthCheck_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/dr0th3r/learnscape/internal/healthCheck"
+)
+
+func TestHandler(t *testing.T) {
+	t.Run("health check returns 200", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, "/health_check", nil)
+		res := httptest.NewRecorder()
+
+		hcheck.HandleHealthCheck().ServeHTTP(res, req)
+
+		statusGot := res.Result().StatusCode
+		statusWant := 200
+		if statusGot != statusWant {
+			t.Errorf("got %q, want %q", statusGot, statusWant)
+		}
+
+		bodyGot := res.Body.String()
+		bodyWant := ""
+		if bodyGot != bodyWant {
+			t.Errorf("got %q, want %q", bodyGot, bodyWant)
+		}
+
+	})
+}
