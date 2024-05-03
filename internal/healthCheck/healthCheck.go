d6c8018dbaf53a6d439a720c1167f65024b24e69
dr0th3r <dr0th3r.code@gmail.com>
2024-05-03 08:01:38 +0200

    feat: add exporting to jaeger

--- internal/healthCheck/healthCheck.go
+++ internal/healthCheck/healthCheck.go
@@ -1,6 +1,7 @@
 package hcheck
 
 import (
+	"fmt"
 	"net/http"
 
 	"go.opentelemetry.io/otel"
@@ -10,6 +11,7 @@ import (
 func HandleHealthCheck() http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println(r.Context())
 			_, span := otel.Tracer("health_chechk").Start(r.Context(), "check")
 			defer span.End()
 			span.SetAttributes(attribute.String("test", "idk"))
--- internal/utils/otel.go
+++ internal/utils/otel.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"errors"
 
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 )
 
@@ -58,20 +61,35 @@ func newPropagator() propagation.TextMapPropagator {
 }
 
 func newTraceProvider() (*trace.TracerProvider, error) {
-	traceExporter, err := stdouttrace.New(
-		stdouttrace.WithPrettyPrint())
+	resource, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("learnscape"),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	/*traceExporter, err := stdouttrace.New(
+	stdouttrace.WithPrettyPrint())*/
+	traceExporter, err := otlptracehttp.New(context.Background())
+
 	if err != nil {
 		return nil, err
 	}
 
 	traceProvider := trace.NewTracerProvider(
 		trace.WithBatcher(traceExporter),
+		trace.WithResource(resource),
 	)
 	return traceProvider, nil
 }
 
 func newMeterProvider() (*metric.MeterProvider, error) {
-	metricExporter, err := stdoutmetric.New()
+	metricExporter, err := stdoutmetric.New(
+		stdoutmetric.WithPrettyPrint())
 	if err != nil {
 		return nil, err
 	}
